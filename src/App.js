77ec71f691666787169560596bc7b4f88e6b86cb
Cláudio de Oliveira Pereira <claudio.pereira@inpa.gov.br>
2021-09-29 14:25:31 -0400

    feat: add App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "@fontsource/roboto";
+import Routes from "./routes";
+import GlobalStyle from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  );
+};
+
+export default App;
